63694f08677a2e97f760e00722f852a9aec3be23
Mocrotronics <aouali@gmail.com>
2018-10-10 17:14:30 +0200

    updated repository Schip

--- src/main/java/nl/hva/projectewa/teamc/repository/SchipRepository.java
+++ src/main/java/nl/hva/projectewa/teamc/repository/SchipRepository.java
@@ -4,6 +4,8 @@ import nl.hva.projectewa.teamc.model.Schip;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class SchipRepository {
 
@@ -21,5 +23,15 @@ public class SchipRepository {
 
     public void delete(Long id) {
         em.remove(em.getReference(Schip.class, id));
+    }
+
+    public List<Schip> findAll() {
+        TypedQuery<Schip> query = em.createQuery("SELECT s from Schip s order by s.typeSchip", Schip.class);
+        return query.getResultList();
+    }
+
+    public Long countAll() {
+        TypedQuery<Long> query = em.createQuery("SELECT count(s) from Schip s", Long.class);
+        return query.getSingleResult();
     }
 }
