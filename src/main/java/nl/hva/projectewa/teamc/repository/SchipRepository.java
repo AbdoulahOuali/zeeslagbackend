83cc1779c17ff6a8384b89e34297ef89bb9f0ddf
Mocrotronics <aouali@gmail.com>
2018-10-10 20:36:29 +0200

    updated repository Schip

--- src/main/java/nl/hva/projectewa/teamc/repository/SchipRepository.java
+++ src/main/java/nl/hva/projectewa/teamc/repository/SchipRepository.java
@@ -5,22 +5,30 @@ import nl.hva.projectewa.teamc.model.Schip;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 import java.util.List;
 
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+@Transactional(SUPPORTS)
 public class SchipRepository {
 
     @PersistenceContext(unitName = "zeeslagPersistenceUnit")
     private EntityManager em;
 
     public Schip find(Long id) {
+
         return em.find(Schip.class, id);
     }
 
+    @Transactional(REQUIRED)
     public Schip create(Schip schip) {
         em.persist(schip);
         return schip;
     }
 
+    @Transactional(REQUIRED)
     public void delete(Long id) {
         em.remove(em.getReference(Schip.class, id));
     }
